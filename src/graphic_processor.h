326ba2c95b24557a042f2b3a6177dfd3d8a30384
Laercio Rios <contact@laerciorios.com>
2024-05-21 21:44:55 -0300

    feat: first version of graphic processor header

---
+++ src/graphic_processor.h
@@ -0,0 +1,12 @@
+#ifndef GRAPHIC_PROCESSOR_H
+#define GRAPHIC_PROCESSOR_H
+
+void write_register(void);
+
+void write_sprite(void);
+
+void write_background(void);
+
+void define_polygon(void);
+
+#endif // GRAPHIC_PROCESSOR_H
