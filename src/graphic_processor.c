3dc4b42fed8302941d9a18063ac1a6b3a411ca58
Laercio Rios <contact@laerciorios.com>
2024-06-07 16:50:03 -0300

    feat: change header

---
+++ src/graphic_processor.c
@@ -0,0 +1,16 @@
+#include "graphic_processor.h"
+
+int
+set_sprite(int registrador, int x, int y, int offset, int activation_bit) {}
+
+int
+set_background_block(int column, int line, int R, int G, int B) {}
+
+int
+set_background_color(int R, int G, int B) {}
+
+void
+increase_coordinate(Sprite* sp, int mirror) {}
+
+int
+collision(Sprite* sp1, Sprite* sp2) {}
--- src/graphic_processor.h
+++ src/graphic_processor.h
@@ -1,12 +1,31 @@
 #ifndef GRAPHIC_PROCESSOR_H
 #define GRAPHIC_PROCESSOR_H
 
-void write_register(void);
+#define LEFT         0
+#define RIGHT        4
+#define UP           2
+#define DOWN         6
+#define UPPER_RIGHT  1
+#define UPPER_LEFT   3
+#define BOTTOM_LEFT  5
+#define BOTTOM_RIGHT 7
 
-void write_sprite(void);
+typedef struct {
+    int coord_x, coord_y;
+    int direction, offset, data_register;
+    int step_x, step_y;
+    int ativo, collision;
+} Sprite;
 
-void write_background(void);
+typedef struct {
+    int coord_x, coord_y, offset;
+    int data_register, ativo;
+} Sprite_Fixed;
 
-void define_polygon(void);
+int set_sprite(int registrador, int x, int y, int offset, int activation_bit);
+int set_background_block(int column, int line, int R, int G, int B);
+int set_background_color(int R, int G, int B);
+void increase_coordinate(Sprite* sp, int mirror);
+int collision(Sprite* sp1, Sprite* sp2);
 
 #endif // GRAPHIC_PROCESSOR_H
